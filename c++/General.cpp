0b95beb97ea4aca065254f0c0b745eccfcab2a40
bucky1995 <xinbo1995@outlook.com>
2019-08-25 21:08:46 -0400

    structure change

--- c++/General.cpp
+++ c++/General.cpp
@@ -32,4 +32,22 @@ public:
         }
         return true;
     }
+
+    int numDecodings(string s) {
+        int n = s.size();
+        int n0=1;
+        int n1=(s[0]=='0')?0:1;
+        for(int i=1;i<n;i++)
+        {
+            int tmp=n1;
+            if(s[i]=='0')
+                n1=0;
+            if((s[i-1]=='2'&&s[i]<='6')||s[i-1]=='1')
+                n1+=n0;
+            if(n1==0)
+                return 0;
+            n0=tmp;
+        }
+        return n1;
+    }
 };
